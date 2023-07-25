27a8d5c3645919c28c28d6b7e01e22640f1a8171
Chidi <chidiebubeokoye755@gmail.com>
2023-07-25 16:04:23 +0100

    final commit

---
+++ tasks/task_four.js
@@ -0,0 +1,35 @@
+##JavaScript Week One Lesson Two Tasks
+
+##How to run/test this project
+
+##ensure you have git installed properly on your machine
+
+##ensure you have node js installed in your machine
+
+##open your terminal and run "git clone "
+
+##run "npm install"
+
+##run "npm run lint:all" to run all lint error, "npm run lint:css" for css error check and "npm run lint:html" for html error check
+
+##To run your Javascript files on your terminal using nodemon, first install nodemon globally using npm install -g nodemon then run your files using nodemon
+
+##To test your lint check for Javascript run "npm run js-lint"
+
+##To fix your lint error/warnings for javascript files run "npm run js-lint-fix"
+
+##Acknowledgement
+
+##Mr Tony Dilibe
+
+##Mrs Ngozi
+
+##Mr Ekene
+
+##Mr Johnson Promise (JP)
+
+##Mr Okechukwu Promise
+
+##Mr Patrick
+
+##This is a work of LM TechHub.
